17f999b0b5cb5e13bb1419fba052cb8db0e1f1fa
Vladimir <vladimir2399@mail.ru>
2023-06-06 11:34:59 +0300

    Add. gitignore file

---
+++ Test_new.py
@@ -0,0 +1,22 @@
+import pytest
+from selene.support.shared import browser
+from selene import be, have
+@pytest.fixture()
+def before_each():
+    print("called before each test")
+def test_first(before_each):
+    assert 1 == 1
+
+def test_two(before_each):
+    assert 1 == 1 ,'ошибОчка'
+
+def test_three(before_each):
+    pass
+browser.open('https://google.com')
+browser.element('[type="search"]').should(be.blank).type('https://www.dota2.com').press_enter()
+browser.element('[id="search"]').should(have.text('dota2.com'))
+
+
+
+
+
